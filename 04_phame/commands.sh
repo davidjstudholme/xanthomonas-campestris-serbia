a9f40b6865db0cd358d8c2b6dab5827276739d93
David Studholme <d.j.studholme@exeter.ac.uk>
2024-05-24 14:44:51 +0100

    Update commands.sh

--- 04_phame/commands.sh
+++ 04_phame/commands.sh
@@ -1,7 +1,10 @@
-screen
+### This takes a while to run, so best to do it in a screen session
+#screen
 
-conda activate phame_env
+### Already installed PhaME into a Conda environment
+#conda activate phame_env
 
+### Assumes that PhaME is already installed
 phame ./phame.fasttree.ctl
 
 
