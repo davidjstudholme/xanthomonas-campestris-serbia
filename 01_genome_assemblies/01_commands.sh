9795e30b44c333ab7b1dcf90e81770ecc77dcf31
David Studholme <d.j.studholme@exeter.ac.uk>
2024-06-21 11:12:52 +0100

    Update 01_commands.sh

--- 01_genome_assemblies/01_commands.sh
+++ 01_genome_assemblies/01_commands.sh
@@ -1,5 +1,6 @@
 ### Download NCBI's datasets utility:
-curl -o datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v1/linux-amd64/datasets'
+curl -o datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-amd64/datasets'
+
 chmod u+x datasets
 
 ### Use NCBI's datasets utility to download the genome sequences, unzip them and make symlinks in curren directory:
