14c8413a41ca0ff47e2f9ec21591a2c8ede7918e
David Studholme <d.j.studholme@exeter.ac.uk>
2024-06-21 11:16:07 +0100

    Update 01_commands.sh

--- 02_ref/01_commands.sh
+++ 02_ref/01_commands.sh
@@ -3,11 +3,8 @@ curl -o datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linu
 chmod u+x datasets
 
 ### Use NCBI's datasets utility to download reference genome sequence
-./datasets download genome accession --inputfile xanthomonas_assm_accs.txt  --exclude-gff3 --exclude-protein --exclude-rna --exclude-genomic-cds --filename xanthomonas_genome_assemblies.zip
+./datasets download genome accession --inputfile xanthomonas_assm_accs.txt --include genome --filename xanthomonas_genome_assemblies.zip
 
 ### Unzip the downloaded file and symlink to current directory
 unzip xanthomonas_genome_assemblies.zip
 ln -s ncbi_dataset/data/GCA_*/GCA_*.fna .
-
-
-
