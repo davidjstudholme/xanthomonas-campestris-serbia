5cad26f0a6cf3545a767743328284bde12630fd3
David Studholme <d.j.studholme@exeter.ac.uk>
2024-06-21 11:15:21 +0100

    Update 01_commands.sh

--- 01_genome_assemblies/01_commands.sh
+++ 01_genome_assemblies/01_commands.sh
@@ -1,13 +1,11 @@
 ### Download NCBI's datasets utility:
 curl -o datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-amd64/datasets'
-
 chmod u+x datasets
 
 ### Use NCBI's datasets utility to download the genome sequences, unzip them and make symlinks in curren directory:
-./datasets download genome accession --inputfile xanthomonas_assm_accs.txt  --exclude-gff3 --exclude-protein --exclude-rna --exclude-genomic-cds --filename xanthomonas_genome_assemblies.zip
+./datasets download genome accession --inputfile xanthomonas_assm_accs.txt --include genome --filename xanthomonas_genome_assemblies.zip
 unzip xanthomonas_genome_assemblies.zip
 ln -s ncbi_dataset/data/GCA_*/GCA_*.fna .
 
 ### Make symlinks to the genome sequence files such that symlinks have informative names and appropriate extensions for input to PhaME:
 perl rename_files.pl genomes.txt
-
