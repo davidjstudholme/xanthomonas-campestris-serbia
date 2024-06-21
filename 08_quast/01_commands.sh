ad323c0150cdd8c65ac14ed7da7fd2ea55d26c1f
David Studholme <d.j.studholme@exeter.ac.uk>
2024-06-21 11:17:37 +0100

    Update 01_commands.sh

--- 08_quast/01_commands.sh
+++ 08_quast/01_commands.sh
@@ -3,7 +3,7 @@ curl -o datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linu
 chmod u+x datasets
 
 ### Use NCBI's datasets utility to download genome sequences
-./datasets download genome accession --inputfile xanthomonas_assm_accs.txt  --exclude-gff3 --exclude-protein --exclude-rna --exclude-genomic-cds --filename xanthomonas_genome_assemblies.zip
+./datasets download genome accession --inputfile xanthomonas_assm_accs.txt  --include genome --filename xanthomonas_genome_assemblies.zip
 
 ### Unzip the the downloaded files and symlink to current directory
 unzip xanthomonas_genome_assemblies.zip
