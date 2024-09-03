8606067f0b7867f7536126f2f16d5a0941c95193
David Studholme <d.j.studholme@exeter.ac.uk>
2024-09-03 09:01:31 +0000

    Started FastANI analysis of genomes, as requested by peer-reviewer

---
+++ 10_ANI/commands.sh
@@ -0,0 +1,4 @@
+### FastANI has already been installed via Conda
+# conda activate fastani_env
+fastANI --ql query_list.txt --rl ref_list.txt -o all-versus-fastANI.short.out -t 6 --visualize --matrix
+
