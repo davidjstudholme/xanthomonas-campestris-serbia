c4b0c2c2a02015d5e025ef1613bf550b56c98a1e
David Studholme <d.j.studholme@exeter.ac.uk>
2024-09-04 08:29:38 +0000

    Cleared-up unnecessary files.

--- 11_circlator/commands.sh
+++ 11_circlator/commands.sh
@@ -1,5 +1,18 @@
 ### Set start position of each circular sequence to the traG gene
 for i in *.fasta; do echo $i; circlator fixstart --genes_fa traG.fna $i $i.fixstart; done
 
+### Annotate each of the fixed FASTA files to generate GenBank files for use with EasyFig
+# conda activate bakta_env
+for i in *.fixstart.fasta; do echo $i; bakta -v --genus Xanthomonas --complete --plasmid unnamed $i; done
+
+
+
+
+### Clean up un-necessary files
+rm *fixstart.tsv *fixstart.svg *.promer *.hypotheticals.tsv *.hypotheticals.faa *.json *.png *.embl *.ffn *.gff3 *contigs_with_ends.fa *gff
+
+
+
+
 
 
